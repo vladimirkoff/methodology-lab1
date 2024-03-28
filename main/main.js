a0bdcdb7f653e2003b96939dba1157cbae35e436
Vladimir Kovalev <vovakovalev2016@icloud.com>
2024-03-28 22:06:31 +0200

    add format parsing in main

--- main/main.js
+++ main/main.js
@@ -6,18 +6,58 @@ import { transform } from "./transformer.js";
 
 const pDir = '..'
 const out = '--out'
+const format = '--format'
+const separator = '='
 
-const processMarkdown = async (filePath) => {
+const processMarkdown = async (args) => {
+  const argv = parseArguments(args.slice(2));
+  const filePath = argv.in;
+  const fileFormat = argv.format;
+
+  if (!filePath) {
+    console.error('Please provide a file path.');
+    return;
+  }
+
   const url = new URL(import.meta.url);
   const dir = path.dirname(url.pathname);
   const parentDir = path.join(dir, pDir);
-  const absolutePath = path.join(parentDir, filePath[2]);
+  const absolutePath = path.join(parentDir, filePath[0]);
   const data = await readFile(absolutePath);
-  const html = transform(data);
+  const html = transform(data, fileFormat);
 
-  if (filePath.find(item => item === out))
-  { const outPath = path.join(parentDir,filePath[4]); await writeFile(outPath, html) }
-  else { console.log(html) }
+  if (argv.out) {
+    const outPath = path.join(parentDir, argv.out);
+    await writeFile(outPath, html);
+  } else {
+    console.log(html);
+  }
 };
 
-processMarkdown(process.argv)
+const parseArguments = (args) => {
+  const argv = {
+    in: args.shift(),
+    out: '',
+    format: '',
+  };
+
+  for (let i = 0; i < args.length; i++) {
+    const arg = args[i];
+    if (arg === out) {
+      i++
+      argv.out = args[i];
+      if (argv.out === undefined) {
+        console.error('Please provide a file path for the output.');
+        process.exit(1);
+      }
+    } else if (arg.startsWith(format)) {
+      argv.format = arg.split(separator)[1];
+    }
+  }
+
+  return argv;
+};
+
+processMarkdown(process.argv);
+
+
